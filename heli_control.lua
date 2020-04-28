08d749accc5005bb84a306aead5738b1b85d49c3
Alexsandro Percy <alexsandro.percy@gmail.com>
2020-04-28 13:40:31 -0300

    added HUD for some in flight information like altitude and time of day

---
+++ heli_control.lua
@@ -0,0 +1,171 @@
+--global constants
+
+gravity = tonumber(minetest.settings:get("movement_gravity")) or 9.8
+tilting_speed = 1
+tilting_max = 0.35
+power_max = 15
+power_min = 0.2 -- if negative, the helicopter can actively fly downwards
+wanted_vert_speed = 10
+
+vector_up = vector.new(0, 1, 0)
+vector_forward = vector.new(0, 0, 1)
+
+function vector_length_sq(v)
+	return v.x * v.x + v.y * v.y + v.z * v.z
+end
+
+last_time = minetest.get_us_time()
+
+function get_pointer_angle(energy)
+    local angle = energy * 18
+    angle = angle - 90
+    angle = angle * -1
+	return angle
+end
+
+if not minetest.global_exists("matrix3") then
+	dofile(minetest.get_modpath("helicopter") .. DIR_DELIM .. "matrix.lua")
+end
+
+function check_node_below(obj)
+	local pos_below = obj:get_pos()
+	pos_below.y = pos_below.y - 0.1
+	local node_below = minetest.get_node(pos_below).name
+	local nodedef = minetest.registered_nodes[node_below]
+	local touching_ground = not nodedef or -- unknown nodes are solid
+			nodedef.walkable or false
+	local liquid_below = not touching_ground and nodedef.liquidtype ~= "none"
+	return touching_ground, liquid_below
+end
+
+function heli_control(self, dtime, touching_ground, liquid_below, vel_before)
+	local driver = minetest.get_player_by_name(self.driver_name)
+	if not driver then
+		-- there is no driver (eg. because driver left)
+		self.driver_name = nil
+		if self.sound_handle then
+			minetest.sound_stop(self.sound_handle)
+			self.sound_handle = nil
+		end
+		self.object:set_animation_frame_speed(0)
+		-- gravity
+		self.object:set_acceleration(vector.multiply(vector_up, -gravity))
+		return
+	end
+
+    
+    --update hud
+    if ((minetest.get_us_time() - last_time) / 1000) > 200 then
+        last_time = minetest.get_us_time()
+        update_heli_hud(driver)
+    end
+
+	local ctrl = driver:get_player_control()
+	local rot = self.object:get_rotation()
+
+	local vert_vel_goal = 0
+	if not liquid_below then
+		if ctrl.jump then
+			vert_vel_goal = vert_vel_goal + wanted_vert_speed
+		end
+		if ctrl.sneak then
+			vert_vel_goal = vert_vel_goal - wanted_vert_speed
+		end
+	else
+		vert_vel_goal = wanted_vert_speed
+	end
+
+	-- rotation
+	if not touching_ground then
+		local tilting_goal = vector.new()
+		if ctrl.up then
+			tilting_goal.z = tilting_goal.z + 1
+		end
+		if ctrl.down then
+			tilting_goal.z = tilting_goal.z - 1
+		end
+		if ctrl.right then
+			tilting_goal.x = tilting_goal.x + 1
+		end
+		if ctrl.left then
+			tilting_goal.x = tilting_goal.x - 1
+		end
+		tilting_goal = vector.multiply(vector.normalize(tilting_goal), tilting_max)
+
+		-- tilting
+		if vector_length_sq(vector.subtract(tilting_goal, self.tilting)) > (dtime * tilting_speed)^2 then
+			self.tilting = vector.add(self.tilting,
+					vector.multiply(vector.direction(self.tilting, tilting_goal), dtime * tilting_speed))
+		else
+			self.tilting = tilting_goal
+		end
+		if vector_length_sq(self.tilting) > tilting_max^2 then
+			self.tilting = vector.multiply(vector.normalize(self.tilting), tilting_max)
+		end
+		local new_up = vector.new(self.tilting)
+		new_up.y = 1
+		new_up = vector.normalize(new_up) -- this is what vector_up should be after the rotation
+		local new_right = vector.cross(new_up, vector_forward)
+		local new_forward = vector.cross(new_right, new_up)
+		local rot_mat = matrix3.new(
+			new_right.x, new_up.x, new_forward.x,
+			new_right.y, new_up.y, new_forward.y,
+			new_right.z, new_up.z, new_forward.z
+		)
+		rot = matrix3.to_pitch_yaw_roll(rot_mat)
+
+		rot.y = driver:get_look_horizontal()
+
+	else
+		rot.x = 0
+		rot.z = 0
+		self.tilting.x = 0
+		self.tilting.z = 0
+	end
+
+	self.object:set_rotation(rot)
+
+	-- calculate how strong the heli should accelerate towards rotated up
+	local power = vert_vel_goal - vel_before.y + gravity * dtime
+	power = math.min(math.max(power, power_min * dtime), power_max * dtime)
+    local touching_ground, liquid_below = check_node_below(self.object)
+
+
+    -- calculate energy consumption --
+    ----------------------------------
+    if self.energy > 0 and touching_ground == false then
+        local position = self.object:get_pos()
+        local altitude_consumption_variable = 0
+
+        -- if gaining altitude, it consumes more power
+        local y_pos_reference = position.y - 200 --after altitude 200 the power need will increase
+        if y_pos_reference > 0 then altitude_consumption_variable = ((y_pos_reference/1000)^2) end
+
+        local consumed_power = (power/1800) + altitude_consumption_variable
+        self.energy = self.energy - consumed_power;
+
+        local energy_indicator_angle = get_pointer_angle(self.energy)
+        self.pointer:set_attach(self.object,'',{x=0,y=11.26,z=9.37},{x=0,y=0,z=energy_indicator_angle})
+    end
+    if self.energy <= 0 then
+        power = 0.2
+		if touching_ground or liquid_below then
+            --criar uma fucao pra isso pois ela repete na linha 268
+			-- sound and animation
+			minetest.sound_stop(self.sound_handle)
+			self.object:set_animation_frame_speed(0)
+			-- gravity
+			self.object:set_acceleration(vector.multiply(vector_up, -gravity))
+		end
+    end
+    ----------------------------
+    -- end energy consumption --
+
+
+	local rotated_up = matrix3.multiply(matrix3.from_pitch_yaw_roll(rot), vector_up)
+	local added_vel = vector.multiply(rotated_up, power)
+	added_vel = vector.add(added_vel, vector.multiply(vector_up, -gravity * dtime))
+	return vector.add(vel_before, added_vel)
+end
+
+
--- init.lua
+++ init.lua
@@ -2,17 +2,17 @@
 -- constants
 --
 
-local tilting_speed = 1
-local tilting_max = 0.35
-local power_max = 15
-local power_min = 0.2 -- if negative, the helicopter can actively fly downwards
-local wanted_vert_speed = 10
 local friction_air_quadratic = 0.01
 local friction_air_constant = 0.2
 local friction_land_quadratic = 1
 local friction_land_constant = 2
 local friction_water_quadratic = 0.1
 local friction_water_constant = 1
+
+--dofile(minetest.get_modpath(minetest.get_current_modname()) .. DIR_DELIM .. "heli_hud.lua")
+dofile(minetest.get_modpath("helicopter") .. DIR_DELIM .. "heli_hud.lua")
+dofile(minetest.get_modpath("helicopter") .. DIR_DELIM .. "heli_control.lua")
+dofile(minetest.get_modpath("helicopter") .. DIR_DELIM .. "fuel_management.lua")
 
 --
 -- helpers and co.
@@ -23,31 +23,6 @@ if not minetest.global_exists("matrix3") then
 end
 
 local creative_exists = minetest.global_exists("creative")
-local gravity = tonumber(minetest.settings:get("movement_gravity")) or 9.8
-local vector_up = vector.new(0, 1, 0)
-local vector_forward = vector.new(0, 0, 1)
-
-local function vector_length_sq(v)
-	return v.x * v.x + v.y * v.y + v.z * v.z
-end
-
-local function get_pointer_angle(energy)
-    local angle = energy * 18
-    angle = angle - 90
-    angle = angle * -1
-	return angle
-end
-
-local function check_node_below(obj)
-	local pos_below = obj:get_pos()
-	pos_below.y = pos_below.y - 0.1
-	local node_below = minetest.get_node(pos_below).name
-	local nodedef = minetest.registered_nodes[node_below]
-	local touching_ground = not nodedef or -- unknown nodes are solid
-			nodedef.walkable or false
-	local liquid_below = not touching_ground and nodedef.liquidtype ~= "none"
-	return touching_ground, liquid_below
-end
 
 local function check_is_under_water(obj)
 	local pos_up = obj:get_pos()
@@ -56,161 +31,6 @@ local function check_is_under_water(obj)
 	local nodedef = minetest.registered_nodes[node_up]
 	local liquid_up = nodedef.liquidtype ~= "none"
 	return liquid_up
-end
-
-local function load_fuel(self, player_name)
-    if self.energy < 9.5 then 
-        local player = minetest.get_player_by_name(player_name)
-        local inv = player:get_inventory()
-        local fuel, inventory_fuel
-        inventory_fuel = "biofuel:biofuel"
-        if inv:contains_item("main", inventory_fuel) then
-            local stack    = ItemStack(inventory_fuel .. " 1")
-            local taken = inv:remove_item("main", stack)
-
-	        self.energy = self.energy + 1
-            local energy_indicator_angle = get_pointer_angle(self.energy)
-            self.pointer:set_attach(self.object,'',{x=0,y=11.26,z=9.37},{x=0,y=0,z=energy_indicator_angle})
-
-	        --sound and animation
-            -- first stop all
-	        minetest.sound_stop(self.sound_handle)
-	        self.sound_handle = nil
-	        self.object:set_animation_frame_speed(0)
-            -- start now
-	        self.sound_handle = minetest.sound_play({name = "helicopter_motor"},
-			        {object = self.object, gain = 2.0, max_hear_distance = 32, loop = true,})
-	        self.object:set_animation_frame_speed(30)
-	        -- disable gravity
-	        self.object:set_acceleration(vector.new())
-            --
-	    end
-    else
-        print("Full tank.")
-    end
-end
-
-local function heli_control(self, dtime, touching_ground, liquid_below, vel_before)
-	local driver = minetest.get_player_by_name(self.driver_name)
-	if not driver then
-		-- there is no driver (eg. because driver left)
-		self.driver_name = nil
-		if self.sound_handle then
-			minetest.sound_stop(self.sound_handle)
-			self.sound_handle = nil
-		end
-		self.object:set_animation_frame_speed(0)
-		-- gravity
-		self.object:set_acceleration(vector.multiply(vector_up, -gravity))
-		return
-	end
-
-	local ctrl = driver:get_player_control()
-	local rot = self.object:get_rotation()
-
-	local vert_vel_goal = 0
-	if not liquid_below then
-		if ctrl.jump then
-			vert_vel_goal = vert_vel_goal + wanted_vert_speed
-		end
-		if ctrl.sneak then
-			vert_vel_goal = vert_vel_goal - wanted_vert_speed
-		end
-	else
-		vert_vel_goal = wanted_vert_speed
-	end
-
-	-- rotation
-	if not touching_ground then
-		local tilting_goal = vector.new()
-		if ctrl.up then
-			tilting_goal.z = tilting_goal.z + 1
-		end
-		if ctrl.down then
-			tilting_goal.z = tilting_goal.z - 1
-		end
-		if ctrl.right then
-			tilting_goal.x = tilting_goal.x + 1
-		end
-		if ctrl.left then
-			tilting_goal.x = tilting_goal.x - 1
-		end
-		tilting_goal = vector.multiply(vector.normalize(tilting_goal), tilting_max)
-
-		-- tilting
-		if vector_length_sq(vector.subtract(tilting_goal, self.tilting)) > (dtime * tilting_speed)^2 then
-			self.tilting = vector.add(self.tilting,
-					vector.multiply(vector.direction(self.tilting, tilting_goal), dtime * tilting_speed))
-		else
-			self.tilting = tilting_goal
-		end
-		if vector_length_sq(self.tilting) > tilting_max^2 then
-			self.tilting = vector.multiply(vector.normalize(self.tilting), tilting_max)
-		end
-		local new_up = vector.new(self.tilting)
-		new_up.y = 1
-		new_up = vector.normalize(new_up) -- this is what vector_up should be after the rotation
-		local new_right = vector.cross(new_up, vector_forward)
-		local new_forward = vector.cross(new_right, new_up)
-		local rot_mat = matrix3.new(
-			new_right.x, new_up.x, new_forward.x,
-			new_right.y, new_up.y, new_forward.y,
-			new_right.z, new_up.z, new_forward.z
-		)
-		rot = matrix3.to_pitch_yaw_roll(rot_mat)
-
-		rot.y = driver:get_look_horizontal()
-
-	else
-		rot.x = 0
-		rot.z = 0
-		self.tilting.x = 0
-		self.tilting.z = 0
-	end
-
-	self.object:set_rotation(rot)
-
-	-- calculate how strong the heli should accelerate towards rotated up
-	local power = vert_vel_goal - vel_before.y + gravity * dtime
-	power = math.min(math.max(power, power_min * dtime), power_max * dtime)
-    local touching_ground, liquid_below = check_node_below(self.object)
-
-
-    -- calculate energy consumption --
-    ----------------------------------
-    if self.energy > 0 and touching_ground == false then
-        local position = self.object:get_pos()
-        local altitude_consumption_variable = 0
-
-        -- if gaining altitude, it consumes more power
-        local y_pos_reference = position.y - 200 --after altitude 200 the power need will increase
-        if y_pos_reference > 0 then altitude_consumption_variable = ((y_pos_reference/1000)^2) end
-
-        local consumed_power = (power/1800) + altitude_consumption_variable
-        self.energy = self.energy - consumed_power;
-
-        local energy_indicator_angle = get_pointer_angle(self.energy)
-        self.pointer:set_attach(self.object,'',{x=0,y=11.26,z=9.37},{x=0,y=0,z=energy_indicator_angle})
-    end
-    if self.energy <= 0 then
-        power = 0.2
-		if touching_ground or liquid_below then
-            --criar uma fucao pra isso pois ela repete na linha 268
-			-- sound and animation
-			minetest.sound_stop(self.sound_handle)
-			self.object:set_animation_frame_speed(0)
-			-- gravity
-			self.object:set_acceleration(vector.multiply(vector_up, -gravity))
-		end
-    end
-    ----------------------------
-    -- end energy consumption --
-
-
-	local rotated_up = matrix3.multiply(matrix3.from_pitch_yaw_roll(rot), vector_up)
-	local added_vel = vector.multiply(rotated_up, power)
-	added_vel = vector.add(added_vel, vector.multiply(vector_up, -gravity * dtime))
-	return vector.add(vel_before, added_vel)
 end
 
 local function get_hipotenuse_value(point1, point2)
@@ -426,6 +246,9 @@ minetest.register_entity("helicopter:heli", {
 			-- gravity
 			self.object:set_acceleration(vector.multiply(vector_up, -gravity))
 
+            --remove hud
+            remove_heli_hud(clicker)
+        
 		elseif not self.driver_name then
             local is_under_water = check_is_under_water(self.object)
             if is_under_water then return end
@@ -527,29 +350,4 @@ if minetest.get_modpath("default") then
 	})
 end
 
---
--- fuel
---
-minetest.register_entity('helicopter:pointer',{
-initial_properties = {
-	physical = false,
-	collide_with_objects=false,
-	pointable=false,
-	visual = "mesh",
-	mesh = "pointer.b3d",
-	textures = {"clay.png"},
-	},
-	
-on_activate = function(self,std)
-	self.sdata = minetest.deserialize(std) or {}
-	if self.sdata.remove then self.object:remove() end
-end,
-	
-get_staticdata=function(self)
-  	
-  self.sdata.remove=true
-  return minetest.serialize(self.sdata)
-end,
-	
-})
 
