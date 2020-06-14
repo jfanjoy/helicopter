e23a17aa189d1a76404c5ecd5a37471bef80d10e
Alexsandro Percy <alexsandro.percy@gmail.com>
2020-06-14 17:32:19 -0300

    change the variable of player name sent to load_fuel

--- init.lua
+++ init.lua
@@ -346,7 +346,7 @@ minetest.register_entity("helicopter:heli", {
 
         if is_attached == true and touching_ground and item_name == "biofuel:biofuel" then
             --refuel
-            load_fuel(self, name)
+            load_fuel(self, self.owner)
         end
 
         if is_attached == false then
