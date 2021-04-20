e3ce04c107851e1d5d30e9eea644d2d20ab9e39d
Jordan Irwin <antumdeluge@gmail.com>
2021-04-19 19:14:02 -0700

    Add setting to enable mouse control by default

--- settings.lua
+++ settings.lua
@@ -1,6 +1,12 @@
 
 --- Determines handling of punched helicopter.
 --
--- If `false`, helicopter is destroyed. Otherwise, it is added to inventory.
---   - Default: false
+--  If `false`, helicopter is destroyed. Otherwise, it is added to inventory.
+--    - Default: false
 helicopter.pick_up = minetest.settings:get_bool("helicopter.pick_up", false)
+
+--- Determines default control of helicopter.
+--
+--  Use facing direction to turn instead of a/d keys by default.
+--    - Default: false
+helicopter.turn_player_look = minetest.settings:get_bool("mount_turn_player_look", false)
