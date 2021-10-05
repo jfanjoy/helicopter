91445b785fe7f720e00510573e6919cb6e3c9235
Alexsandro Percy <alexsandro.percy@gmail.com>
2021-10-05 11:47:00 -0300

    now start rotation by mouse

--- heli_entities.lua
+++ heli_entities.lua
@@ -68,7 +68,7 @@ minetest.register_entity("helicopter:heli", {
     hp_max = 50,
     color = "#0063b0",
     _passenger = nil,
-    _by_mouse = helicopter.turn_player_look,
+    _by_mouse = true, --helicopter.turn_player_look,
 
     get_staticdata = function(self) -- unloaded/unloads ... is now saved
         return minetest.serialize({
